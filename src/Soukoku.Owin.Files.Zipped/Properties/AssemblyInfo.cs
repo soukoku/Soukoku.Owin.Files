b4e77ad2f3f0b5788ff39ec0cded57fd04599369
Eugene Wang <yinchun.wang@gmail.com>
2015-08-01 17:40:00 -0400

    Added description.

--- src/Soukoku.Owin.Files.Zipped/Properties/AssemblyInfo.cs
+++ src/Soukoku.Owin.Files.Zipped/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Soukoku.Owin.Files.Zipped")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Allows serving files from a zipped store.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("Soukoku.Owin.Files.Zipped")]
 [assembly: AssemblyTrademark("")]
--- src/Soukoku.Owin.Files/Properties/AssemblyInfo.cs
+++ src/Soukoku.Owin.Files/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Soukoku.Owin.Files")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Simple owin file serving middleware.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("Soukoku.Owin.Files")]
 [assembly: AssemblyTrademark("")]
