911f7d3663b2675743a66eaf5135b1a241cd1164
Eugene Wang <yinchun.wang@gmail.com>
2015-08-06 17:33:05 -0400

    Owin header class as IDictionary.

--- src/Soukoku.Owin.Files/OwinHeaders.cs
+++ src/Soukoku.Owin.Files/OwinHeaders.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -10,21 +11,24 @@ namespace Soukoku.Owin.Files
     /// <summary>
     /// Provides very basic methods to work with http headers.
     /// </summary>
-    public class OwinHeaders
+    public class OwinHeaders : IDictionary<string, string[]>
     {
-        IDictionary<string, string[]> _store;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="OwinHeaders"/> class.
         /// </summary>
-        /// <param name="store">The store.</param>
+        /// <param name="rawHeaders">The store.</param>
         /// <exception cref="System.ArgumentNullException">store</exception>
-        public OwinHeaders(IDictionary<string, string[]> store)
+        public OwinHeaders(IDictionary<string, string[]> rawHeaders)
         {
-            if (store == null) { throw new ArgumentNullException("store"); }
+            if (rawHeaders == null) { throw new ArgumentNullException("rawHeaders"); }
 
-            _store = store;
+            _raw = rawHeaders;
         }
+
+        IDictionary<string, string[]> _raw;
+
+        #region custom header tools
 
         /// <summary>
         /// Gets the <see cref="System.String"/> with the specified header.
@@ -39,7 +43,7 @@ namespace Soukoku.Owin.Files
             get
             {
                 string[] test;
-                if (_store.TryGetValue(header, out test) && test != null)
+                if (_raw.TryGetValue(header, out test) && test != null)
                 {
                     return string.Join(",", test);
                 }
@@ -57,11 +61,11 @@ namespace Soukoku.Owin.Files
         {
             if (string.IsNullOrEmpty(value))
             {
-                _store.Remove(header);
+                _raw.Remove(header);
             }
             else
             {
-                _store[header] = new[] { value };
+                _raw[header] = new[] { value };
             }
         }
 
@@ -75,17 +79,17 @@ namespace Soukoku.Owin.Files
         {
             if (values == null || values.Length == 0)
             {
-                _store.Remove(header);
+                _raw.Remove(header);
             }
             else
             {
                 if (collapse)
                 {
-                    _store[header] = new[] { string.Join(",", values) };
+                    _raw[header] = new[] { string.Join(",", values) };
                 }
                 else
                 {
-                    _store[header] = values;
+                    _raw[header] = values;
                 }
             }
         }
@@ -100,18 +104,178 @@ namespace Soukoku.Owin.Files
             if (values == null || values.Length == 0) { return; }
 
             string[] old;
-            if (_store.TryGetValue(header, out old))
+            if (_raw.TryGetValue(header, out old))
             {
                 string[] final = new string[old.Length + values.Length];
                 Array.Copy(old, final, old.Length);
                 Array.Copy(values, 0, final, old.Length, values.Length);
-                _store[header] = final;
+                _raw[header] = final;
             }
             else
             {
-                _store[header] = values;
+                _raw[header] = values;
             }
         }
+
+        #endregion
+
+        #region idictionary
+
+        public bool ContainsKey(string key)
+        {
+            return _raw.ContainsKey(key);
+        }
+
+        /// <summary>
+        /// Adds the specified key.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        public void Add(string key, string[] value)
+        {
+            _raw.Add(key, value);
+        }
+
+        /// <summary>
+        /// Removes the specified key.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns></returns>
+        public bool Remove(string key)
+        {
+            return _raw.Remove(key);
+        }
+
+        public bool TryGetValue(string key, out string[] value)
+        {
+            return _raw.TryGetValue(key, out value);
+        }
+
+        /// <summary>
+        /// Adds the specified item.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        public void Add(KeyValuePair<string, string[]> item)
+        {
+            _raw.Add(item);
+        }
+
+        /// <summary>
+        /// Removes all items from the <see cref="T:System.Collections.Generic.ICollection`1" />.
+        /// </summary>
+        public void Clear()
+        {
+            _raw.Clear();
+        }
+
+        public bool Contains(KeyValuePair<string, string[]> item)
+        {
+            return _raw.Contains(item);
+        }
+
+        public void CopyTo(KeyValuePair<string, string[]>[] array, int arrayIndex)
+        {
+            _raw.CopyTo(array, arrayIndex);
+        }
+
+        /// <summary>
+        /// Removes the specified item.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        /// <returns></returns>
+        public bool Remove(KeyValuePair<string, string[]> item)
+        {
+            return _raw.Remove(item);
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        /// An enumerator that can be used to iterate through the collection.
+        /// </returns>
+        public IEnumerator<KeyValuePair<string, string[]>> GetEnumerator()
+        {
+            return _raw.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through a collection.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="T:System.Collections.IEnumerator" /> object that can be used to iterate through the collection.
+        /// </returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _raw.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Gets an <see cref="T:System.Collections.Generic.ICollection`1" /> containing the keys of the <see cref="T:System.Collections.Generic.IDictionary`2" />.
+        /// </summary>
+        public ICollection<string> Keys
+        {
+            get
+            {
+                return _raw.Keys;
+            }
+        }
+
+        /// <summary>
+        /// Gets an <see cref="T:System.Collections.Generic.ICollection`1" /> containing the values in the <see cref="T:System.Collections.Generic.IDictionary`2" />.
+        /// </summary>
+        public ICollection<string[]> Values
+        {
+            get
+            {
+                return _raw.Values;
+            }
+        }
+
+        /// <summary>
+        /// Gets the number of elements contained in the <see cref="T:System.Collections.Generic.ICollection`1" />.
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                return _raw.Count;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the <see cref="T:System.Collections.Generic.ICollection`1" /> is read-only.
+        /// </summary>
+        public bool IsReadOnly
+        {
+            get
+            {
+                return _raw.IsReadOnly;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the <see cref="System.String[]"/> with the specified key.
+        /// </summary>
+        /// <value>
+        /// The <see cref="System.String[]"/>.
+        /// </value>
+        /// <param name="key">The key.</param>
+        /// <returns></returns>
+        string[] IDictionary<string, string[]>.this[string key]
+        {
+            get
+            {
+                return _raw[key];
+            }
+
+            set
+            {
+                _raw[key] = value;
+            }
+        }
+
+        #endregion
 
         #region common headers
 
